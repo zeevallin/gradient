acacf9d80882146336aecc90237072a992fdc2b3
Philip Vieira <zee@vall.in>
2015-11-19 11:25:11 +0100

    Be specific in grd spec about asserting color

--- spec/gradient/grd_spec.rb
+++ spec/gradient/grd_spec.rb
@@ -3,19 +3,19 @@ RSpec.describe Gradient::GRD do
   def assert_maps(maps)
     expect(maps).to match a_hash_including("Kiwi")
     # TODO: Reconcile with python results by gradient in original Photoshop export
-    # assert_point(maps["Kiwi"].color_points[0], 0.0, 60, 17, 3)
-    assert_point(maps["Kiwi"].color_points[0], 0.0, 61, 17, 3)
+    # assert_color_point(maps["Kiwi"].color_points[0], 0.0, 60, 17, 3)
+    assert_color_point(maps["Kiwi"].color_points[0], 0.0, 61, 17, 3)
     # TODO: Reconcile with python results by gradient in original Photoshop export
-    # assert_point(maps["Kiwi"].color_points[1], 0.386, 41, 133, 12) FROM PYTHON RESULTS
-    assert_point(maps["Kiwi"].color_points[1], 0.386, 41, 134, 13)
+    # assert_color_point(maps["Kiwi"].color_points[1], 0.386, 41, 133, 12) FROM PYTHON RESULTS
+    assert_color_point(maps["Kiwi"].color_points[1], 0.386, 41, 134, 13)
     # TODO: Reconcile with python results by gradient in original Photoshop export
-    # assert_point(maps["Kiwi"].color_points[2], 0.84, 159, 202, 27) FROM PYTHON RESULTS
-    assert_point(maps["Kiwi"].color_points[2], 0.84, 160, 203, 27)
-    assert_point(maps["Kiwi"].color_points[3], 0.927, 243, 245, 110)
-    assert_point(maps["Kiwi"].color_points[4], 1.0, 255, 255, 255)
+    # assert_color_point(maps["Kiwi"].color_points[2], 0.84, 159, 202, 27) FROM PYTHON RESULTS
+    assert_color_point(maps["Kiwi"].color_points[2], 0.84, 160, 203, 27)
+    assert_color_point(maps["Kiwi"].color_points[3], 0.927, 243, 245, 110)
+    assert_color_point(maps["Kiwi"].color_points[4], 1.0, 255, 255, 255)
   end
 
-  def assert_point(point, location, red, green, blue)
+  def assert_color_point(point, location, red, green, blue)
     expect(point.location.round(3)).to eq location.round(3)
     expect(color_to_rgb(point.color)).to eq color_to_rgb(Color::RGB.new(red, green, blue))
   end
