91870f674afdb79d48d2a1027a48c5ba7ee2a79f
J.J. Green <jim@aframe.com>
2015-12-29 19:21:25 +0000

    svg fixture specs now use a shared helper for DRY

--- spec/gradient/svg_spec.rb
+++ spec/gradient/svg_spec.rb
@@ -12,130 +12,89 @@ RSpec.describe Gradient::SVG do
     File.open(fixture_path(base), 'r').read
   end
 
-  describe '.parse' do
+  shared_examples_for 'a hash of Gradient::Map' do |expect|
 
-    context 'subtle.svg' do
+    let(:map_hash) { described_class.parse(fixture_buffer(fixture_name)) }
 
-      # this a simple single gradient svg, no surprises
+    it 'should be a Hash' do
+      expect(map_hash).to be_a Hash
+    end
 
-      let(:maps) { described_class.parse(fixture_buffer('subtle')) }
+    it 'should have the correct keys' do
+      expect(map_hash.keys).to match_array(expect.keys)
+    end
 
-      it 'should return a hash' do
-        expect(maps).to be_a Hash
-      end
+    expect.keys.each do |key|
 
-      it 'should have the correct keys' do
-        expect(maps.keys).to match_array ['subtle']
-      end
+      describe "the map with key '#{key}'" do
 
-      describe 'the hash value' do
-
-        let(:map) { maps['subtle'] }
+        let(:map) { map_hash[key] }
 
         it 'should be a Gradient::Map' do
           expect(map).to be_a Gradient::Map
         end
 
-        describe 'the points' do
-
-          let(:points) { map.points }
-
-          it 'should be an array' do
-            expect(points).to be_an Array
-          end
-
-          it 'should have 11 elements' do
-            expect(points.count).to eq 11
-          end
-
-          it 'should consist of Gradient::Point objects' do
-            points.each do |point|
-              expect(point).to be_a Gradient::Point
-            end
-          end
-
-          describe 'the locations' do
-
-            let(:locations) { points.map(&:location) }
-
-            it 'should have the expected values' do
-              expected =
-                [0.00, 0.06, 0.19, 0.25,
-                 0.32, 0.42, 0.53, 0.65,
-                 0.75, 0.87, 1.0]
-              expect(locations).to match_array expected
-            end
+        it "should have #{expect[key]} points" do
+          expect(map.points.count).to eq expect[key]
+          map.points.each do |point|
+            expect(point).to be_a Gradient::Point
           end
         end
       end
     end
+  end
+
+  describe '.parse' do
+
+    # this a simple single gradient svg, no surprises
+
+    context 'subtle.svg' do
+      it_should_behave_like('a hash of Gradient::Map', 
+                            'subtle' => 11 ) do
+        let(:fixture_name) { 'subtle' }
+      end
+    end
+
+    # this a single gradient with various sorts of valid stop-color
+    # values, exercises the parsing of colors
 
     context 'hard-colors.svg' do
-
-      # this a single gradient with various sorts of valid stop-color
-      # values, exercises the parsing of colors
-
-      let(:maps) { described_class.parse(fixture_buffer('hard-colors')) }
-
-      it 'should return a hash' do
-        expect(maps).to be_a Hash
+      it_should_behave_like('a hash of Gradient::Map', 
+                            'hard-colors' => 5 ) do
+        let(:fixture_name) { 'hard-colors' }
       end
-
-      it 'should have the correct keys' do
-        expect(maps.keys).to match_array ['hard-colors']
-      end
-
     end
+
+    # this valid svg file has no gradients in it
 
     context 'no-gradient.svg' do
-
-      # this valid svg file has no gradients in it
-
-      let(:maps) { described_class.parse(fixture_buffer('no-gradient')) }
-
-      it 'should return a hash' do
-        expect(maps).to be_a Hash
+      it_should_behave_like('a hash of Gradient::Map', {}) do
+        let(:fixture_name) { 'no-gradient' }
       end
-
-      it 'should have the correct keys' do
-        expect(maps.keys).to match_array []
-      end
-
     end
+
+    # this specifies color and opacity via a css-style attribute
+    # using named colors; also, does not specify the svg namespace
 
     context 'lemon-lime.svg' do
-
-      # this specifies color and opacity via a css-style attribute
-      # using named colors; also, does not specify the svg namespace
-
-      let(:maps) { described_class.parse(fixture_buffer('lemon-lime')) }
-
-      it 'should return a hash' do
-        expect(maps).to be_a Hash
+      it_should_behave_like('a hash of Gradient::Map', 
+                            'Lemon-Lime' => 5) do
+        let(:fixture_name) { 'lemon-lime' }
       end
-
-
-      it 'should have the correct keys' do
-        expect(maps.keys).to match_array ['Lemon-Lime']
-      end
-
     end
 
+    # contains multiple gradients, css-style with hex colors, and
+    # several gradients which are links (which do not have any stops
+    # of their own, so should be ignored)
+
     context 'punasia.svg' do
-
-      # contains multiple gradients, css-style with hex colors, and
-      # several gradients which are links (which do not have any stops
-      # of their own, so should be ignored)
-
-      let(:maps) { described_class.parse(fixture_buffer('punasia')) }
-
-      it 'should return a hash' do
-        expect(maps).to be_a Hash
-      end
-
-      it 'should have the correct keys' do
-        ids = [281, 333, 336, 356, 393].map { |id| "linearGradient#{id}" }
-        expect(maps.keys).to match_array ids
+      it_should_behave_like('a hash of Gradient::Map', 
+                            'linearGradient281' => 2, 
+                            'linearGradient333' => 2, 
+                            'linearGradient336' => 2, 
+                            'linearGradient356' => 2, 
+                            'linearGradient393' => 2) do
+        let(:fixture_name) { 'punasia' }
       end
     end
   end
